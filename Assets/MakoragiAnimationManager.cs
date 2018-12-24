8e228537674cb95ba86e2ac270fc05f57b08fc7d
SatoshiRobatoFujimoto <good_2_go@live.jp>
2018-12-24 17:47:36 +0900

    initial commit

---
+++ Assets/MakoragiAnimationManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakoragiAnimationManager : MonoBehaviour {
+
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("is_jumping");
+    }
+}
